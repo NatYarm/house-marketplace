d76602ce43e6d1821d7c2ad3e0fdbe0c4e60947b
NatYarm <yarmnat7@gmail.com>
2023-04-02 21:55:25 +0300

    Add sign up functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
+import { db } from '../firebase.config';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -22,6 +28,34 @@ const SignUp = () => {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      console.log(auth);
+
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      console.log(userCredential);
+
+      const user = userCredential.user;
+
+      console.log(user);
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+    } catch (error) {}
+
+    navigate('/');
+  };
+
   return (
     <>
       <div className="pageContainer">
@@ -29,7 +63,7 @@ const SignUp = () => {
           <p className="pageHeader">Welcome Back!</p>
         </header>
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="text"
               className="nameInput"
