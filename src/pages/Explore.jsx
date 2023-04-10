37332cbae1ccdbe6fb469b9f9e08dec478fa724a
NatYarm <yarmnat7@gmail.com>
2023-04-10 21:34:50 +0300

    Explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -8,6 +8,29 @@ const Explore = () => {
       <header>
         <p className="pageHeader">Explore</p>
       </header>
+
+      <main>
+        {/* Slider */}
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+          <Link to="/category/sell">
+            <img
+              src={sellCategoryImage}
+              alt="sell"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for sale</p>
+          </Link>
+        </div>
+      </main>
     </div>
   );
 };
